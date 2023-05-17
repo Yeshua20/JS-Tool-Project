e201d8d936ca04bbb4eaa6379c6e9147c13b9322
fua21 <129446635+fua21@users.noreply.github.com>
2023-05-17 10:18:37 +0300

    additional variables

--- game/game.js
+++ game/game.js
@@ -2,3 +2,7 @@ const playerText=document.querySelector("#playerText");
 const computerText=document.querySelector("#computerText");
 const resultText=document.querySelector("#resultText");
 const choiceBtn=document.querySelectorAll(".choiceBtn");
+
+let player;
+let computer;
+let result;
